5332ab7671fd73883e59d2da4a8f1294ae4e1baf
Milan Krcadinac <milan.krcadinac@gmail.com>
2025-10-30 02:06:06 +0100

    Fix: Switch to traditional Convex + Clerk auth

--- convex/auth.ts
+++
@@ -1,7 +0,0 @@
-// convex/auth.ts
-import { convexAuth } from "@convex-dev/auth/server";
-import { ConvexCredentials } from "@convex-dev/auth/providers/ConvexCredentials";
-
-export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [ConvexCredentials],
-});
